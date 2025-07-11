4aa79f405e5de43372c5d4b485c924a91c134fd5
eunji-0508 <cojia@naver.com>
2025-07-11 20:44:30 +0900

    Hold

--- src/main/java/org/example/lv2/Calculator.java
+++ src/main/java/org/example/lv2/Calculator.java
@@ -8,8 +8,8 @@ import java.util.List;
 
 // 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성
 public class Calculator {
-    // 속성
-    // 나중에 있을수도 있는 변환 때 편하게 하기위해 List로 작성함 (사실 상관없음)
+    // 속성(필드)
+    // 나중에 변환하게 되면 편하게 하기 위해 List로 작성함 (사실 지금은 상관없음)
     private List<Integer> resultsList = new ArrayList<>();
 
     private int num1;
@@ -17,10 +17,11 @@ public class Calculator {
     private char operator;
     private int result;
 
-    // 생성자
+    // 생성자 - 기본 생성자 사용함
 
     // 기능
     // 사칙연산을 수행 후, 결과값 반환 메서드 구현
+    // 여기에는 매개변수 안줘도 되네
     public int calculate() {
         try {
             switch (operator) {
@@ -38,18 +39,13 @@ public class Calculator {
 
                 case '/':
                     if (num2 == 0) {
-                        return -1;
+                        throw new ArithmeticException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                     }
                     result = num1 / num2;
                     break;
-
-                default:
-                    System.out.println("+, -, *, / 중에 다시 입력하세요!");
-                    return -1;
             }
         } catch (ArithmeticException e) {
-            System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-            return -1;
+            System.out.println(e.getMessage()); // 에러 메시지 출력 기능
         }
         resultsList.add(result);
         return result;
@@ -57,8 +53,9 @@ public class Calculator {
 
     // 컬렉션의 가장 먼저 저장된 데이터를 삭제하는 기능
     // ArrayList는 순서 유지하기 때문에 첫번째 값 삭제할 수 있음
-     // 트러블슈팅- IndexOutOfBoundsException
-    public void removedFirst () {
+    // 트러블슈팅- IndexOutOfBoundsException
+    // 값 없는데 참조할 거라고 생각을 안함
+    public void removeFirstResult () {
         if(resultsList.isEmpty()) {
             System.out.println("저장된 데이터가 없습니다.");
         } else {
@@ -66,14 +63,15 @@ public class Calculator {
         }
     }
 
-//    // 삭제되었는지 확인하는 메서드도 필요한가?
-//    public void getResultsList() {
-//        System.out.println("resultsList = " + resultsList);
-//    }
+    // 삭제되었는지 확인하는 메서드도 필요한가?
+    // main 메서드에서 Calculator 클래스의 연산 결과를 저장하고 있는 컬렉션 필드에 직접 접근하지 못하도록 수정 (캡슐화)
+    public List<Integer> getResultsList() {
+        return new ArrayList<>(resultsList);
+    }
 
-    //  main 메서드에서 Calculator 클래스의 연산 결과를 저장하고 있는 컬렉션 필드에 직접 접근하지 못하도록 수정 (캡슐화)
     // Getter 메서드
     public int getResult() {
+        System.out.println("계산 결과: " + result);
         return result;
     }
 
--- src/main/java/org/example/lv2/Main.java
+++ src/main/java/org/example/lv2/Main.java
@@ -27,10 +27,32 @@ public class Main {
                 continue;   // 다시 while문으로 돌아가서 반복
             }
 
+            scanner.nextLine();     // 트러블슈팅: nextInt()는 숫자만 읽음. 엔터 안먹어서 charAt(0)에서 오류남. 라인 정리하는 게 해결책!
+            System.out.println();   // 깔끔하게 보기 위해 띄어쓰기 추가
 
+            char operator;    // 트러블슈팅: ''안에 공백 하나 넣어줘야 오류 안남(빈 문자 표현 x). 공백은 문자로 처리되기 때문임. 없애도 되네!
 
+            // 사칙연산 기호(+, -, *, /)를 입력받기
+            while (true) {
+                System.out.print("사칙연산 기호를 입력하세요(+, -, *, /): ");
 
+                // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장하기 (charAt(0))
+                // charAt(0) - 첫 문자만 꺼내겠다
+                // String operator =  scanner.next();
+                operator = scanner.nextLine().charAt(0);    // 트러블슈팅
 
+                if(operator == '+' || operator == '-' || operator == '*' || operator == '/') {
+                    break;
+                } else {
+                    System.out.println("+, -, *, / 중에 다시 입력하세요!");
+                }
+            }
+
+            calculator.setResult(num1, num2, operator);
+
+            calculator.calculate();
+
+            calculator.getResult();
 
             // 반복문을 사용하되, 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정하기
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
@@ -39,6 +61,10 @@ public class Main {
                 break;
             }
         }
+        System.out.println("연산 결과 리스트: " + calculator.getResultsList());
+        calculator.removeFirstResult();
+        System.out.println("연산 결과 리스트: " + calculator.getResultsList());
+
         scanner.close();    // 스캐너 닫기
     }
 
